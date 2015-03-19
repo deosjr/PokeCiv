a7c34b8bd7c0702104e76e94e12c2b2b9249a020
Sjoerd Dost <deosjr@gmail.com>
2015-03-19 17:03:29 +0100

    General Structure
    I'll start on battles now and fill in the details as I go.

--- Model/Pokemon/Move.cs
+++ Model/Pokemon/Move.cs
@@ -8,5 +8,28 @@ namespace PokeCiv.Model.Pokemon
 {
     class Move
     {
+
+        public int id;
+        public string name;
+        private string functioncode;
+        private int power;
+        private PokemonType type;
+        private string category;
+
+        public Move(string[] data)
+        {
+            id = Convert.ToInt32(data[0]);
+            name = data[2];
+            functioncode = data[3];
+            power = Convert.ToInt32(data[4]);
+            type = Types.getType(data[5]);
+            category = data[6];
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+
     }
 }
---
+++ Model/Pokemon/Moves.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeCiv.Model.Pokemon
+{
+    class Moves
+    {
+        private static Dictionary<string, int> internalNames = new Dictionary<string, int>();
+        private static List<Move> moves = new List<Move>();
+
+        static Moves()
+        {
+            load_moves();
+        }
+
+        private static void load_moves()
+        {
+
+            string datapath = "../../Data/moves.txt";
+            var lines = File.ReadAllLines(datapath);
+            foreach (var line in lines)
+            {
+                string[] features = line.Split(',');
+                Move move = new Move(features);
+                moves.Add(move);
+                internalNames.Add(features[1], move.id);
+            }
+        }
+
+        public static Move getType(string name) 
+        {
+            return moves[internalNames[name] - 1];
+        }
+
+        public static Move getType(int id)
+        {
+            return moves[id - 1];
+        }
+    }
+}
