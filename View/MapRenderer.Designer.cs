6c4a13577427053b44f74d0fb12e3d428b93fbf7
bigbadkip <leovanexxen@gmail.com>
2015-04-08 11:54:04 +0200

    Fixes basic map rendering

--- View/MapRenderer.Designer.cs
+++ View/MapRenderer.Designer.cs
@@ -28,34 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.mapBackgroundCanvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mapBackgroundCanvas)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(755, 450);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.mapBackgroundCanvas.Location = new System.Drawing.Point(0, -1);
+            this.mapBackgroundCanvas.Name = "pictureBox1";
+            this.mapBackgroundCanvas.Size = new System.Drawing.Size(755, 450);
+            this.mapBackgroundCanvas.TabIndex = 0;
+            this.mapBackgroundCanvas.TabStop = false;
             // 
             // MapRenderer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(758, 449);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.mapBackgroundCanvas);
             this.Name = "MapRenderer";
             this.Text = "Route101";
             this.Load += new System.EventHandler(this.MapRenderer_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mapBackgroundCanvas)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox mapBackgroundCanvas;
     }
 }
