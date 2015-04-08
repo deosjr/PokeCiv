f4d6ec53acf51b495255ee6497c44faf58bf3456
bigbadkip <leovanexxen@gmail.com>
2015-04-08 14:13:54 +0200

    debugplayer

--- View/MapRenderer.Designer.cs
+++ View/MapRenderer.Designer.cs
@@ -28,8 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MapRenderer));
             this.mapBackgroundCanvas = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.mapBackgroundCanvas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // mapBackgroundCanvas
@@ -41,16 +44,29 @@
             this.mapBackgroundCanvas.TabIndex = 0;
             this.mapBackgroundCanvas.TabStop = false;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(350, 200);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
             // MapRenderer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(758, 449);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.mapBackgroundCanvas);
             this.Name = "MapRenderer";
             this.Text = "Route101";
             this.Load += new System.EventHandler(this.MapRenderer_Load);
             ((System.ComponentModel.ISupportInitialize)(this.mapBackgroundCanvas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -58,5 +74,6 @@
         #endregion
 
         private System.Windows.Forms.PictureBox mapBackgroundCanvas;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
