70bb5ccd0f105d1ca485df91e77da87da73a5470
bigbadkip <leovanexxen@gmail.com>
2015-04-08 10:36:50 +0200

    1e tussenversie van een map visualiseren

---
+++ View/Routes/Route101.cs
@@ -0,0 +1,27 @@
+﻿using PokeCiv.Model.World;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokeCiv.View
+{
+    public partial class Route101 : Form
+    {
+        public Route101()
+        {
+            InitializeComponent();
+        }
+
+        private void Route101_Load(object sender, EventArgs e)
+        {
+            Map map = new Map();
+           //public Array[][] tile = map.getTileData();
+        
+    }
+}
