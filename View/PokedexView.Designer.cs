66c6215a38301c56d43e07da34f796c062c777ce
bigbadkip <leovanexxen@gmail.com>
2015-04-13 13:39:45 +0200

    omdat het kan, pokedex 2.0

--- View/PokedexView.Designer.cs
+++ View/PokedexView.Designer.cs
@@ -43,16 +43,16 @@
             this.PokemonListBox.DataSource = this.pokedexViewBindingSource;
             this.PokemonListBox.DisplayMember = "Location";
             this.PokemonListBox.FormattingEnabled = true;
-            this.PokemonListBox.Location = new System.Drawing.Point(12, 12);
+            this.PokemonListBox.Location = new System.Drawing.Point(178, 246);
             this.PokemonListBox.Name = "PokemonListBox";
-            this.PokemonListBox.Size = new System.Drawing.Size(152, 381);
+            this.PokemonListBox.Size = new System.Drawing.Size(191, 160);
             this.PokemonListBox.TabIndex = 1;
             this.PokemonListBox.ValueMember = "BackColor";
             this.PokemonListBox.SelectedIndexChanged += new System.EventHandler(this.PokemonListBox_SelectedIndexChanged);
             // 
             // pokedexToMapButton
             // 
-            this.pokedexToMapButton.Location = new System.Drawing.Point(467, 370);
+            this.pokedexToMapButton.Location = new System.Drawing.Point(467, 383);
             this.pokedexToMapButton.Name = "pokedexToMapButton";
             this.pokedexToMapButton.Size = new System.Drawing.Size(75, 23);
             this.pokedexToMapButton.TabIndex = 2;
@@ -60,14 +60,14 @@
             this.pokedexToMapButton.UseVisualStyleBackColor = true;
             this.pokedexToMapButton.Click += new System.EventHandler(this.pokedexToMapButton_Click);
             // 
-            // pictureBox1
+            // pokedexPokemonImage
             // 
-            this.pokedexPokemonImage.BackColor = System.Drawing.Color.White;
+            this.pokedexPokemonImage.BackColor = System.Drawing.Color.Transparent;
             this.pokedexPokemonImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pokedexPokemonImage.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pokedexPokemonImage.Location = new System.Drawing.Point(379, 32);
-            this.pokedexPokemonImage.Name = "pictureBox1";
-            this.pokedexPokemonImage.Size = new System.Drawing.Size(151, 151);
+            this.pokedexPokemonImage.Image = ((System.Drawing.Image)(resources.GetObject("pokedexPokemonImage.Image")));
+            this.pokedexPokemonImage.Location = new System.Drawing.Point(178, 27);
+            this.pokedexPokemonImage.Name = "pokedexPokemonImage";
+            this.pokedexPokemonImage.Size = new System.Drawing.Size(191, 151);
             this.pokedexPokemonImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pokedexPokemonImage.TabIndex = 3;
             this.pokedexPokemonImage.TabStop = false;
@@ -77,10 +77,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(554, 428);
             this.Controls.Add(this.pokedexPokemonImage);
             this.Controls.Add(this.pokedexToMapButton);
             this.Controls.Add(this.PokemonListBox);
+            this.DoubleBuffered = true;
             this.Name = "PokedexView";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.PokedexView_Load);
