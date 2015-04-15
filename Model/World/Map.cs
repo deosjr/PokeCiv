42a7dd584f1dfc271b2e4944b2588ce261e7068c
Sjoerd Dost <deosjr@gmail.com>
2015-04-15 10:37:05 +0200

    Slightly less ugly hardcoding of Map

--- Model/World/Map.cs
+++ Model/World/Map.cs
@@ -28,41 +28,35 @@ namespace PokeCiv.Model.World
             SetGrid();
         }
 
-        // VERY UGLY right now, but will be prettier when mapgeneration gets done
         public void SetGrid()
         {
-            string _ = "_";
-            string w = "w";
-            string x = "x";
-            string u = "u";
-            string b = "b";
-            string[,] ascii = new string[,]{
-                {x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,_,_,_,_,_},
-                {x,_,_,_,_,x,x,_,_,_,_,x,_,b,_,_,w,w,_,b,_,_,_,_},
-                {x,_,w,w,_,x,x,_,_,_,_,_,_,b,_,w,w,_,_,_,b,_,_,_},
-                {x,_,w,w,_,_,_,_,_,u,u,u,_,b,_,_,_,_,_,_,_,b,_,_},
-                {x,_,w,w,_,_,_,_,u,u,u,u,_,b,_,x,x,_,_,_,_,u,u,_},
-                {x,_,_,_,_,_,_,_,u,u,u,_,_,b,_,x,x,_,_,_,_,u,u,u},
-                {x,x,_,_,_,_,_,_,w,w,w,_,_,b,_,x,x,_,_,_,_,_,_,u},
-                {x,x,_,_,_,b,b,b,w,w,w,_,_,_,_,x,x,_,_,_,_,_,_,u},
-                {x,x,w,_,_,x,x,x,x,w,w,_,_,_,_,x,x,_,_,_,_,x,_,u},
-                {x,w,w,w,_,x,x,w,_,_,w,_,_,_,_,_,_,_,_,_,_,x,_,u},
-                {x,w,w,w,w,w,w,w,_,_,w,_,b,b,b,_,_,_,_,_,_,x,_,u},
-                {x,_,w,w,_,w,w,w,_,_,_,_,_,b,_,_,_,_,_,_,_,x,_,u},
-                {x,_,_,_,_,w,w,w,_,_,_,_,_,b,_,_,_,_,_,_,_,x,_,x},
-                {x,x,_,_,_,_,_,_,_,_,_,_,_,u,u,_,_,_,_,_,_,x,_,u},
-                {x,x,_,_,_,_,b,b,b,b,b,_,_,u,u,_,_,_,_,_,_,x,_,u},
-                {x,_,_,_,_,x,x,_,_,_,_,_,_,_,_,_,_,_,_,_,_,u,u,u},
-                {x,_,w,w,_,x,x,_,_,_,_,_,_,_,_,_,_,_,_,_,_,u,u,u},
-                {x,_,w,w,x,x,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,u,u,u},
-                {x,_,w,w,x,x,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,u,u,u},
-                {x,_,_,_,_,_,_,_,_,_,_,_,x,_,_,_,_,_,_,_,_,u,u,_},
-                {u,u,u,_,_,_,_,u,_,_,_,_,x,_,_,_,_,_,_,_,_,b,_,_},
-                {_,u,u,u,_,_,u,u,_,_,_,_,_,_,_,_,_,_,_,_,_,b,_,_},
-                {_,_,_,u,u,u,u,x,x,x,x,x,x,x,x,x,x,x,x,x,x,b,_,_}
+            string[] ascii = new string[]{
+                "xxxxxxxxxxxxxxxxxxx     ",
+                "x    xx    x b  ww b    ",
+                "x ww xx      b ww   b   ",
+                "x ww     uuu b       b  ",
+                "x ww    uuuu b xx    uu ",
+                "x       uuu  b xx    uuu",
+                "xx      www  b xx      u",
+                "xx   bbbwww    xx      u",
+                "xxw  xxxxww    xx    x u",
+                "xwww xxw  w          x u",
+                "xwwwwwww  w bbb      x u",
+                "x ww www     b       x u",
+                "x    www     b       x x",
+                "xx           uu      x u",
+                "xx    bbbbb  uu      x u",
+                "x    xx              uuu",
+                "x ww xx              uuu",
+                "x wwxx               uuu",
+                "x wwxx               uuu",
+                "x           x        uu ",
+                "uuu    u    x        b  ",
+                " uuu  uu             b  ",
+                "   uuuuxxxxxxxxxxxxxxb  "
             };
 
-            Grid = new Tile[ascii.GetLength(0), ascii.GetLength(1)];
+            Grid = new Tile[ascii.GetLength(0), ascii[0].Length];
 
             // encounters = {0.45: ("PIDGEY", 2, 4), 0.3:("RATTATA",2,2), 0.2:("SENTRET",3,3), 0.05:("FURRET",6,6)}
             ArrayList testWildPokemon = new ArrayList();
@@ -70,25 +64,25 @@ namespace PokeCiv.Model.World
 
             for (int Y = 0; Y < ascii.GetLength(0); Y++)
             {
-                for (int X = 0; X < ascii.GetLength(1); X++)
+                for (int X = 0; X < ascii[0].Length; X++)
                 {
                     Grid[Y,X] = new Tile();
-                    switch (ascii[Y,X]) {
-                        case "_":
+                    switch (ascii[Y].ElementAt(X)) {
+                        case ' ':
                             break;
-                        case "w":
+                        case 'w':
                             Grid[Y,X].tileItem = new Grass(testWildPokemon, 0.1);
                             Grid[Y,X].tileType = "W";
                             break;
-                        case "x":
+                        case 'x':
                             Grid[Y,X].tileItem = new Block();
                             Grid[Y,X].tileType = "X";
                             break;
-                        case "u":
+                        case 'u':
                             Grid[Y,X].tileItem = new Water();
                             Grid[Y,X].tileType = "U";
                             break;
-                        case "b":
+                        case 'b':
                             Grid[Y,X].tileItem = new Block();
                             Grid[Y, X].tileItem.imageSrc = "../../Data/Graphics/Tiles/bush.png";
                             Grid[Y,X].tileType = "B";
